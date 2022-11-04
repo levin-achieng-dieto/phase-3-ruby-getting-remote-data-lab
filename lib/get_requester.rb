e733224a1848c45d3ec8ab7739bd14a7e80ce72e
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-11-04 17:42:57 +0300

    commit tree 722c32f53e358de0e36af1d738a5c55fb4efd8bf

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,24 @@
 # Write your code here
+require 'open-uri'
+require 'net/http'
+require 'json'
+class GetRequester
+
+    def initialize(url)
+        @url = url
+        # @uri = ""
+    end
+
+    def get_response_body
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        responses = JSON.parse(self.get_response_body)
+        responses.collect do |response|
+            response
+        end
+    end
+end
